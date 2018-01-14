733137b1c2bcfcd5540a53cf4ea05f5af64ecb39
James Earlywine <james@esilogix.com>
2018-01-14 10:11:08 -0500

    initial commit
    Hi from the template commit-mag hook

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,25 @@
 <head>
     <meta charset="utf-8">
     <title>@yield('title', $cruddyData['brandName'])</title>
+    <link rel="stylesheet" href="/{!! rtrim(str_replace('index.php', '', config('cruddy.uri_prefix')), '/') . '/' !!}cruddy/bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css" />
+
     {!! $styles !!}
+
+<!-- environment detection-->
+<script src='/{!! rtrim(str_replace('index.php', '', config('cruddy.uri_prefix')), '/') . '/' !!}cruddy/js/environment.js?v=27'></script>
+<script>
+    // if we are hosted in production
+    if (window.indystar_environment.detect() !== 'dev') {
+        // and we are not using https
+        /*
+        if (window.location.protocol != "https:") {
+            // redirect to https
+            window.location.href = "https:" + window.location.href.substring(window.location.protocol.length);
+        }
+        */
+    }
+</script>
+    
 </head>
 <body>
     <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
@@ -29,6 +47,24 @@
     Cruddy = {!! json_encode($cruddyData) !!};
     </script>
 
-    {!! $scripts !!}
+    
+    <script src='/{!! rtrim(str_replace('index.php', '', config('cruddy.uri_prefix')), '/') . '/' !!}cruddy/js/vendor.min.js?v=27'></script>
+    <script src='/{!! rtrim(str_replace('index.php', '', config('cruddy.uri_prefix')), '/') . '/' !!}cruddy/js/app.js?v=27'></script>
+    
+    <!-- this version of bootstrap breaks cruddy drop-down.  
+         Leaving it in (but commented out) case we need to extract some parts to support datetimepicker --jle
+    <script src='/{!! rtrim(str_replace('index.php', '', config('cruddy.uri_prefix')), '/') . '/' !!}cruddy/bower_components/bootstrap/dist/js/bootstrap.min.js?v=27'></script>
+    -->
+    <script src='/{!! rtrim(str_replace('index.php', '', config('cruddy.uri_prefix')), '/') . '/' !!}cruddy/bower_components/moment/moment.js?v=27'></script>
+    <script src='/{!! rtrim(str_replace('index.php', '', config('cruddy.uri_prefix')), '/') . '/' !!}cruddy/bower_components/eonasdan-bootstrap-datetimepicker/src/js/bootstrap-datetimepicker.js?v=27'></script>
+    <style>
+        .form-group.field__datetime {
+            position: relative;
+        }
+    </style>
+    
+    <!-- {!! $scripts !!} -->
+    
+    
 </body>
 </html>
